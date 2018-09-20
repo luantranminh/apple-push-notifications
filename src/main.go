445265f73fc5f226098281af07cc8ee42b09cc6c
luantranminh <15520454@gm.uit.edu.vn>
2018-09-20 10:59:10 +0700

    Send notification to iOS device through APNs

--- src/main.go
+++ src/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/sideshow/apns2"
 	"github.com/sideshow/apns2/certificate"
 )
 
 func main() {
+
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	cert, err := certificate.FromPemFile("./config/pushcert.pem", "")
 	if err != nil {
@@ -16,11 +23,11 @@ func main() {
 	}
 
 	notification := &apns2.Notification{}
-	notification.DeviceToken = "11aa01229f15f0f0c52029d8cf8cd0aeaf2365fe4cebc4af26cd6d76b7919ef7"
-	notification.Topic = "com.sideshow.Apns2"
-	notification.Payload = []byte(`{"aps":{"alert":"Hello!"}}`) // See Payload section below
+	notification.DeviceToken = os.Getenv("DEVICE_TOKEN")
+	notification.Topic = os.Getenv("TOPIC")
+	notification.Payload = []byte(`{"aps":{"alert":"test","sound":"default"}}`) // See Payload section below
 
-	client := apns2.NewClient(cert).Production()
+	client := apns2.NewClient(cert).Development()
 	res, err := client.Push(notification)
 
 	if err != nil {
