3ac857a8313cc213bddc0f1ed51dbdad7e415964
luantranminh <15520454@gm.uit.edu.vn>
2018-09-22 20:02:20 +0700

    Add Payload struct

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -9,6 +10,33 @@ import (
 	"github.com/sideshow/apns2"
 	"github.com/sideshow/apns2/certificate"
 )
+
+type Alert struct {
+	Title string `json:"title,omitempty"`
+	Body  string `json:"body,omitempty"`
+}
+
+type Aps struct {
+	Alert            Alert  `json:"alert,omitempty"`
+	Sound            string `json:"sound,omitempty"`
+	Badge            int    `json:"badge,omitempty"`
+	ContentAvailable int    `json:"content-available,omitempty"`
+}
+
+type CustomData struct {
+	GroupID  string  `json:"group_id"`
+	UserID   string  `json:"user_id"`
+	Lat      float64 `json:"lat"`
+	Lng      float64 `json:"lng"`
+	Accuracy float64 `json:"accuracy"`
+}
+
+type Payload struct {
+	Aps        *Aps        `json:"aps,omitempty"`
+	CustomData *CustomData `json:"data,omitempty"`
+}
+
+const a = "13e63da480a4ed41d9498aa9d5c7f2a153e15aff3c1e71c1774061f79939dd27"
 
 func main() {
 
@@ -22,10 +50,32 @@ func main() {
 		log.Fatal("Cert Error:", err)
 	}
 
+	alert := Alert{
+		Title: "Day la title",
+		Body:  "Day la body",
+	}
+
+	aps := Aps{
+		Alert: alert,
+		Sound: "default",
+	}
+
+	payload := Payload{
+		Aps: &aps,
+	}
+
+	message, err := json.Marshal(payload)
+
+	// log.Fatal(string(message[:len(message)]))
+
+	if err != nil {
+		return
+	}
+
 	notification := &apns2.Notification{}
-	notification.DeviceToken = os.Getenv("DEVICE_TOKEN")
+	notification.DeviceToken = a
 	notification.Topic = os.Getenv("TOPIC")
-	notification.Payload = []byte(`{"aps":{"alert":"test","sound":"default"}}`) // See Payload section below
+	notification.Payload = message
 
 	client := apns2.NewClient(cert).Development()
 	res, err := client.Push(notification)
